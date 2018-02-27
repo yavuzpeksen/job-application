9595ca473df7d549d2f0408e52074dd4aa2cae42
Yavuz <yavuzpeksen1986@hotmail.com>
2018-02-27 16:05:46 +0300

    authorize only admin for JobListingRepository

--- src/main/java/com/kodgemisi/assignment/repositories/JobListingRepository.java
+++ src/main/java/com/kodgemisi/assignment/repositories/JobListingRepository.java
@@ -2,9 +2,12 @@ package com.kodgemisi.assignment.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.kodgemisi.assignment.domains.JobListing;
 
+/*Only ADMIN can use JobListingRepository*/
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface JobListingRepository extends JpaRepository<JobListing, Long>{
 	
 	@Query("SELECT jl FROM JOB_LISTING jl INNER JOIN jl.user u ON u.id = jl.user AND u.email = ?1")
