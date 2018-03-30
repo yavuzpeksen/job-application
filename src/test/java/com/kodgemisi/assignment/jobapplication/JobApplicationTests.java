54fcd21c4395bbc79abbd7d9ba1d1451ed16544b
Yavuz <yavuzpeksen1986@hotmail.com>
2018-03-30 23:59:04 +0300

    Mail integration test is added

--- src/test/java/com/kodgemisi/assignment/jobapplication/JobApplicationTests.java
+++ src/test/java/com/kodgemisi/assignment/jobapplication/JobApplicationTests.java
@@ -2,7 +2,13 @@ package com.kodgemisi.assignment.jobapplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -12,8 +18,17 @@ import com.kodgemisi.assignment.JobApplication;
 @SpringBootTest(classes=JobApplication.class)
 public class JobApplicationTests {
 
+	@Autowired
+	private JavaMailSender mailSender;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws MailException{
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo("asdsa@zcvd.com");
+		email.setSubject("Web App Test email");
+		email.setText("Test Message\n");
+		email.setFrom("noreply@domain.com");
+		mailSender.send(email);
 	}
 
 }
